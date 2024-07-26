887baf624a007e2785135ea746829436a1cec56d
7Alain86 <91161874+7Alain86@users.noreply.github.com>
2024-07-26 16:44:26 -0500

    Update edit.blade.php on email_bien
    Reparado el error de la falta de datos en el apartado de editar estudiantes.

--- email_bien/resources/views/students/edit.blade.php
+++ email_bien/resources/views/students/edit.blade.php
@@ -7,17 +7,18 @@
     @method('PUT')
     <div class="mb-3">
         <label for="name" class="form-label">Nombre:</label>
-        <input type="text" class="form-control" name="name" id="name" required>
+        <input type="text" class="form-control" name="name" id="name" value="{{ $student['name'] }}" required>
     </div>
     <div class="mb-3">
         <label for="apellido" class="form-label">Apellido:</label>
-        <input type="text" class="form-control" name="apellido" id="apellido" required>
+        <input type="text" class="form-control" name="apellido" id="apellido" value="{{ $student['apellido'] }}"  required>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Email:</label>
-        <input type="email" class="form-control" name="email" id="email" required>
+        <input type="email" class="form-control" name="email" id="email" value="{{ $student['email'] }}" required>
     </div>
     <button type="submit" class="btn btn-primary">Guardar</button>
 </form>
+<div> </div>
 <a href="{{ route('students.index') }}">Volver a la lista</a>
 @endsection
